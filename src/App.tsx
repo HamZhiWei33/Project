6e96e62d33749869d2bc1ee844859d651f68d92b
HamZhiWei33 <149857735+HamZhiWei33@users.noreply.github.com>
2024-10-07 19:47:33 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
@@ -12,7 +14,17 @@ function App() {
           base: "1fr",
           lg: "250px 1fr",
         }}
-      ></Grid>
+      >
+        <GridItem bg="tomato" gridArea="nav">
+          <NavBar />
+        </GridItem>
+        <GridItem gridArea="main">
+          <GameGrid />
+        </GridItem>
+        <GridItem bg="lightblue" gridArea="aside">
+          Sidebar
+        </GridItem>
+      </Grid>
     </>
   );
 }
